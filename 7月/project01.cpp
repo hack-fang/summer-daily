6d41ed2dff13f8154d502af9a94b4588958eb284
ChadSZ <18895373521@163.com>
2019-07-22 21:56:07 +0800

    Add files via upload

---
+++ 7月/project01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>//预处理
+using namespace std;//命名空间
+int main(){//主函数
+	cout << "first touch C++,this is a strong computer language" << endl;//cout输出
+	system("pause");
+	return 0;//返回0
+}
+
